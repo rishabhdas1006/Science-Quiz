34b9f830c0cad89990ecabba9ca1dec7a3c7ceea
Rishabh Das <121282518+rishabhdas1006@users.noreply.github.com>
2023-07-23 22:32:09 +0530

    Added initial files

---
+++ questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is a non metal that remains liquid at room temperature?",
+    answer: "Bromine",
+    options: [
+      "Phosphorous",
+      "Bromine",
+      "Chlorine",
+      "Helium"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is used in pencils?",
+    answer: "Graphite",
+    options: [
+      "Graphite",
+      "Silicon",
+      "Charcoal",
+      "Phosphorous"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following metals forms an amalgam with other metals?",
+    answer: "Mercury",
+    options: [
+      "Tin",
+      "Mercury",
+      "Lead",
+      "Zinc"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Chemical formula for water is?",
+    answer: "H2O",
+    options: [
+      "NaAlO<sub>2</sub>",
+      "H<sub>2</sub>O",
+      "Al<sub>2</sub>O<sub>3</sub>",
+      "CaSiO<sub>3</sub>"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Washing soda is the common name for",
+    answer: "Sodium Carbonate",
+    options: [
+      "Sodium Carbonate",
+      "Calcium Bicarbonate",
+      "Sodium Bicarbonate",
+      "Calcium Carbonate"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Potassium Permanganate is used for purifying drinking water, because",
+    answer: "it is an oxidising agent",
+    options: [
+      "it is a sterilising agent",
+      "it dissolves the impurities of water",
+      "it is a reducing agent",
+      "it is an oxidising agent"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following is an element?",
+    answer: "Diamond",
+    options: [
+      "Ruby",
+      "Sapphire",
+      "Emerald",
+      "Diamond"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following is not an isotope of hydrogen?",
+    answer: "Yttrium",
+    options: [
+      "Tritium",
+      "Protium",
+      "Deuterium",
+      "Yttrium"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Production of chlorofluorocarbons (CFC) gas which is proposed to be banned in India, is used in which of the following domestic products?",
+    answer: "Refrigerator",
+    options: [
+      "Television",
+      "Refrigerator",
+      "Tube Light",
+      "Cooking Gas"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Bell metal is an alloy of",
+    answer: "Tin & Copper",
+    options: [
+      "Nickel & Copper",
+      "Zinc & Copper",
+      "Brass & Nickel",
+      "Tin & Copper"
+    ]
+  },
+];
